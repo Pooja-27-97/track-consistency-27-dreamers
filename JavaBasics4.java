e142e1dac8c89a23d2c48848c795654e150c044e
Pooja-27-97 <poojarm2797@gmail.com>
2026-01-28 01:59:19 +0530

    Patterns in java

---
+++ JavaBasics4.java
@@ -0,0 +1,143 @@
+public class JavaBasics4 {
+    public static void hallowRect(int row, int col) {
+        for(int i=1; i <= row; i++) {
+            for(int j=1; j <= col; j++) {
+                if(i == 1||i == row||j == 1||j == col) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void halfPyramid(int row) {
+        for(int i=1; i <= row; i++) {
+            for(int j=1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void invRotatedPy(int row) {
+        for(int i=1; i <= row; i++) {
+            for(int j=1; j <= (row-i); j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void inHalfPyNum(int row) {
+        for(int i=row; i >= 1; i--) {
+            for(int j=1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void floyd(int row) {
+        int num = 1;
+        for(int i=1; i <= row; i++) {
+            for(int j=1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void zeroOne(int row) {
+        for(int i=1; i <= row; i++) {
+            for(int j=1; j <= i; j++) {
+                if((i+j) % 2 == 0) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void butterfly(int row) {
+        for(int i=1; i <= row; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for(int j=1; j <= (row-i)*2; j++) {
+                System.out.print("  ");
+            }
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=row; i >= 1; i--) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for(int j=1; j <= (row-i)*2; j++) {
+                System.out.print("  ");
+            }
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void solidRhombus(int row) {
+        for(int i=1; i <= row; i++) {
+            for(int j =1; j <= (row-i); j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j <= row; j++) {
+                if(i == 1||i == row||j==1||j == row) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void diamond(int row) {
+        for(int i=1; i <= row; i++) {
+            for(int j =1; j <= (row-i); j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j <= (i*2-1); j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=row; i >= 1; i--) {
+            for(int j =1; j <= (row-i); j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j <= (i*2-1); j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        // hallowRect(4, 5);
+        // halfPyramid(4);
+        // invRotatedPy(4);
+        // inHalfPyNum(5);
+        // floyd(5);
+        // zeroOne(5);
+        // butterfly(4);
+        // solidRhombus(5);
+        diamond(4);
+    }
+}
