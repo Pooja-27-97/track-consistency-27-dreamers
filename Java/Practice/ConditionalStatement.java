9356c272329f1fda771098bc0cbc0cbe3ecdd97f
Pooja-27-97 <poojarm2797@gmail.com>
2026-02-09 23:18:45 +0530

    Practicing Java

---
+++ Java/Practice/ConditionalStatement.java
@@ -0,0 +1,76 @@
+import java.util.*;
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Leap year or not
+        // int year = sc.nextInt();
+
+        // if(year % 4 == 0) {
+        //     if(year % 100 == 0) {
+        //         if(year % 400 == 0) {
+        //             System.out.println("Leap year");
+        //         } else {
+        //             System.out.println("Not a leap year");
+        //         }
+        //     } else {
+        //         System.out.println("Leap year");
+        //     }
+        // } else {
+        //     System.out.println("Not a leap year");
+        // }
+
+        //value of x & y 
+        // int a = 63, b = 36;
+        // boolean x = (a < b ) ? true : false;
+        // int y= (a > b ) ? a : b;
+        // System.out.println("Value of x and y: " + x + " " + y);
+
+        //Printing week days
+        // int num = sc.nextInt();
+
+        // switch (num) {
+        //     case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 3:
+        //         System.out.println("Wednesday");
+        //         break;
+        //     case 4:
+        //         System.out.println("Thursday");
+        //         break;
+        //     case 5:
+        //         System.out.println("Friday");
+        //         break;
+        //     case 6:
+        //         System.out.println("Saturday");
+        //         break;
+        //     case 7:
+        //         System.out.println("Sunday");
+        //         break;
+        //     default:
+        //         System.out.println("Invalid inputs");
+        // }
+
+        //Predicting fever
+        // double temp = 103.5;
+        // if(temp > 100) {
+        //     System.out.println("Have fever");
+        // } else {
+        //     System.out.println("Normal");
+        // }
+
+        //Print positive or negative
+        // int num = sc.nextInt();
+        // if(num == 0) {
+        //     System.out.println("Neither positive nor negative");
+        // } else if (num > 0) {
+        //     System.out.println("Positive");
+        // } else {
+        //     System.out.println("Negative");
+        // }
+    }
+}
---
+++ Java/Practice/Variables.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Variables {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Adding tax to the bill
+        // float pencil = sc.nextFloat();
+        // float pen = sc.nextFloat();
+        // float eraser = sc.nextFloat();
+
+        // double total = pencil + pen + eraser;
+        // System.out.println("Total Amount: " + total);
+
+        // float gst = 0.18f;
+        // double tax = (total * gst) + total;
+        // System.out.println("Amount after adding 18% tax: " + tax);
+
+        //area of a square
+        // double side = sc.nextDouble();
+        // double area = side * side;
+        // System.out.println("Area of square is " + area);
+
+        //print average
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // int avg = (a+b+c)/3;
+        // System.out.println("Average : " + avg);
+    }
+}
