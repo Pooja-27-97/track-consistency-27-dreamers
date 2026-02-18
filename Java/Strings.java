a561a118381b9e4454ca400c6fd61b31bc3a8150
Pooja-27-97 <poojarm2797@gmail.com>
2026-02-18 23:07:41 +0530

    Strings

---
+++ Java/Strings.java
@@ -0,0 +1,46 @@
+public class Strings {
+    public static void printLetters(String str) {
+        for(int i=0; i < str.length(); i++) {
+            System.out.print(str.charAt(i) + " ");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        for(int i=0; i < str.length()/2; i++) {
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n-i-1)) return false;
+        }
+        return true;
+    }
+
+    public static void checkPalindrome(String str) {
+        if(isPalindrome(str)) System.out.println("Palindrome");
+        else System.out.println("Not palindrome");
+    }
+
+    public static float findShortestPath(String path) {
+        int x=0, y=0;
+
+        for(int i=0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+            //north
+            if(dir == 'N') y++;
+            else if (dir == 'S') y--;
+            else if (dir == 'W') x--;
+            else x++;
+        }
+        float shortPath = (float)Math.sqrt((x*x) + (y*y));
+        return shortPath;
+    }
+
+    public static void main(String[] args) {
+        // String str = "Hello World";
+        // printLetters(str);
+        // checkPalindrome(str);
+
+        // String path = "WNEENESENN";
+        // System.out.println(findShortestPath(path));
+
+        System.out.println("hello".equals("hello"));
+    }
+}
