f7f7a6aca0d15dab16848c31ef36b4608d137047
Pooja-27-97 <poojarm2797@gmail.com>
2026-02-25 01:56:10 +0530

    Practicing JS

---
+++ JS/practice.js
@@ -0,0 +1,73 @@
+// let num = parseInt(prompt("Enter any integer value"));
+
+// if(num % 10 === 0) console.log("Good");
+// else console.log("Bad");
+
+
+// let name = prompt("Enter your name");
+// let age = prompt("Enter your age");
+
+// alert(`${name} is ${age} year old!`);
+
+
+// let quat = parseInt(prompt("Enter the quarter (1-4)"));
+
+// switch(quat) {
+//     case 1:
+//         console.log("January\nFebruary\nMarch");
+//         break;
+//     case 2:
+//         console.log("April\nMay\nJune");
+//         break;
+//     case 3:
+//         console.log("July\nAugust\nSeptember");
+//         break;
+//     case 4:
+//         console.log("October\nNovember\nDecember");
+//         break;
+//     default:
+//         console.log("Out of range");
+// }
+
+
+// let str = prompt("Enter any string");
+
+// if((str[0] === 'j' || str[0] === 'J') && (str.length >= 8)) console.log("Golden String");
+// else console.log("Not a Golden string");
+
+
+// let num1 = parseInt(prompt("Enter 1st integer"));
+// let num2 = parseInt(prompt("Enter 2nd integer"));
+// let num3 = parseInt(prompt("Enter 3rd integer"));
+
+// let max = (num1 > num2)?((num1 > num3)? num1: num2):((num2 > num3)?num2:num3);
+// console.log(`Largest number: ${max}`);
+
+
+// let num1 = parseInt(prompt("Enter 1st numbers"));
+// let num2 = parseInt(prompt("Enter 2nd numbers"));
+
+// if(num1 % 10 === num2 % 10) console.log("Same last digit");
+// else console.log("Not same");
+
+
+// let arr = [7, 9, 0, -2, 4, 8, 10];
+// let n = 5;
+// console.log(arr.slice(-n));
+
+
+// let str = " ";
+// if(str === " ") console.log("Blank string");
+// else console.log("Not blank");
+
+
+// let str = "dreAmers";
+// let idx = 3;
+// if(str[idx] === str[idx].toLowerCase()) console.log("Lower case");
+// else console.log("Not lower");
+
+
+// let arr = [7, 9, 0, -2, 4, 8, 10];
+// let rand = 4;
+// if(arr.includes(rand)) console.log("Exists in an array");
+// else console.log("Doesn't exists in an array");
