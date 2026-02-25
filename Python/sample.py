060ff6d3d364e374bfe3c73d00c967fb25527504
Pooja-27-97 <poojarm2797@gmail.com>
2026-02-26 00:59:52 +0530

    Conditional statements and loops

--- Python/sample.py
+++ Python/sample.py
@@ -1,3 +1,4 @@
+import random, sys
 # name = input("Enter your name : ")
 # print(len(name))
 # print(int(27.97))
@@ -5,3 +6,56 @@
 # print(89 == '89')
 # print(True or False)
 # print(not False)
+
+# year = 2032
+# if(year % 4 == 0):
+#     if(year % 100 != 0):
+#         print("Leap year")
+#     else:
+#         if(year % 400 == 0):
+#             print("Leap year")
+#         else:
+#             print("Not a leap year")
+# else:
+#     print("Not a leap year")
+
+
+# num = random.randint(1, 51)
+# guess = int(input("Guess the number (1-50): "))
+
+# while(True):
+#     if(guess == num or guess == 0):
+#         sys.exit()
+#     if(guess < num):
+#         guess = int(input("Try with higer value: "))
+#     elif (guess > num):
+#         guess = int(input("Try with lower value: "))
+
+# if(guess == num):
+#     print("Congrats! Right guess")
+# else :
+#     print("Out of the game")
+
+
+# n = 10
+# i = 1
+# while( i <= n):
+#     if(i == 7):
+#         i = i+1
+#         continue
+#     print(i)
+#     i = i+1
+    
+
+# for i in range (10, 0, -2):
+#     print(i)
+
+# sum = 0
+# for i in range (1, 101):
+#     sum =sum +i
+# print(sum)
+
+# fact = 1
+# for i in range(1, 6):
+#     fact = fact * i
+# print(fact)
