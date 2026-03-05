729178ae500dadae9736aa7c0b5e02849526dfae
Pooja-27-97 <poojarm2797@gmail.com>
2026-03-05 21:15:01 +0530

    DOM concepts

--- JS/dom.js
+++ JS/dom.js
@@ -1,21 +1,73 @@
-let btn = document.querySelector("button");
-let h3 = document.querySelector("h3");
+// let btn = document.querySelector("button");
+// let h3 = document.querySelector("h3");
 
-btn.addEventListener("click", function () {
-    let h3 = document.querySelector("h3");
-    let randomColor = getRandomColor();
-    h3.innerText = randomColor;
+// btn.addEventListener("click", function () {
+//     let h3 = document.querySelector("h3");
+//     let randomColor = getRandomColor();
+//     h3.innerText = randomColor;
 
-    let div = document.querySelector('div');
-    div.style.backgroundColor = randomColor;
-    // console.log("Color updated");
-});
+//     let div = document.querySelector('div');
+//     div.style.backgroundColor = randomColor;
+//     // console.log("Color updated");
+// });
 
-function getRandomColor() {
-    let red = Math.floor(Math.random() * 255);
-    let green = Math.floor(Math.random() * 255);
-    let blue = Math.floor(Math.random() * 255);
+// function getRandomColor() {
+//     let red = Math.floor(Math.random() * 255);
+//     let green = Math.floor(Math.random() * 255);
+//     let blue = Math.floor(Math.random() * 255);
 
-    let color = `rgb(${red}, ${green}, ${blue})`;
-    return color;
-}
+//     let color = `rgb(${red}, ${green}, ${blue})`;
+//     return color;
+// }
+
+
+// let h1 = document.getElementById('dream');
+// console.log(h1);
+// h1.style.color = "aqua";
+// h1.style.textDecoration = "underline";
+
+// let elts = document.getElementsByClassName('list');
+// console.log(elts);
+// elts[0].style.color = "orange";
+
+// let heading = document.getElementsByTagName('h2');
+// console.log(heading);
+
+
+// let text = document.querySelectorAll('.list');
+// console.log(text);
+
+// let heading = document.querySelector('h1');
+// heading.innerText = "Hello world";
+
+
+// let element = document.getElementById("demo");
+// console.log(element.textContent);
+// console.log(element.innerText); 
+// console.log(element.innerHTML);
+
+
+// let input = document.createElement('input');
+// document.body.appendChild(input);
+// input.placeholder = "Username";
+// input.type = "password";
+
+// let btn = document.createElement('button');
+// document.body.prepend(btn);
+// btn.style.backgroundColor = "aqua";
+// btn.style.color = "red";
+// btn.innerText = "Click me!";
+// btn.id = "btn";
+
+// let heading = document.createElement('h1');
+// heading.textContent = "DOM practice";
+// heading.style.textDecoration = "underline";
+// heading.style.color = "purple";
+// document.body.appendChild(heading);
+
+
+// let para = document.createElement('p');
+// para.innerHTML = "Dreams are <b>powerful</b>";
+// para.style.color = "purple";
+// document.querySelector('body').append(para);
+// para.classList.add("parastyle");
