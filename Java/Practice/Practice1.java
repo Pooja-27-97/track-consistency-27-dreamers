f235ad64ee43ef753d3eb0aeb3c60587b8ad8d45
Pooja-27 <poojarm2797@gmail.com>
2026-03-26 20:49:12 +0530

    Practied Concepts

--- Java/Practice/Practice1.java
+++ Java/Practice/Practice1.java
@@ -1,10 +1,98 @@
+import java.util.*;
+
 public class Practice1 {
+    // public static boolean isPrime(int num) {
+    // if (num == 2)
+    // return true;
+    // else {
+    // boolean isprime = true;
+
+    // for (int i = 2; i <= Math.sqrt(num); i++) {
+    // if (num % i == 0) {
+    // isprime = false;
+    // }
+    // }
+    // return isprime;
+    // }
+    // }
+
+    // public static void checkPrime(int num) {
+    // if (isPrime(num))
+    // System.out.println("Prime");
+    // else
+    // System.out.println("Not Prime");
+    // }
+
     public static void main(String[] args) {
-        //Max of 3 numbers
+        // Max of 3 numbers
         // int a = 100;
         // int b = 50;
         // int c = 20;
         // int result = (a > b) ? ((a > c) ? a: c): ((b > c)? b: c);
         // System.out.println(result);
+
+        // int a = 10;
+        // int b = 20;
+        // System.out.println(a + " " + b);
+
+        // a = a + b;//30
+        // b = a - b;//10
+        // a = a - b;//20
+        // System.out.println(a + " " + b);
+
+        // Scanner sc = new Scanner(System.in);
+        // int num = sc.nextInt();
+
+        // if (num % 2 == 0)
+        // System.out.println("Even");
+        // else
+        // System.out.println("Odd");
+
+        // int num1 = (int) (Math.random() * 10) + 1;
+        // int num2 = (int) (Math.random() * 10) + 1;
+        // int num3 = (int) (Math.random() * 10) + 1;
+        // System.out.println(num1 + " " + num2 + " " + num3);
+
+        // int result = (num1 > num2) ? ((num1 > num3) ? num1 : num3) : ((num2 > num3) ?
+        // num2 : num3);
+        // System.out.println("Greatest: " + result);
+
+        // double num = 28.258;
+        // System.out.println((int) num);
+
+        // int avg = (num1 + num2 + num3) / 3;
+        // System.out.println("Average: " + avg);
+
+        // int sum = 0;
+        // for (int i = 1; i <= 10; i++) {
+        // sum += i;
+        // }
+        // System.out.println(sum);
+
+        // int num = 12321;
+        // int og = num;
+        // int rev = 0;
+
+        // while (num > 0) {
+        // int lastDigit = num % 10;
+        // rev = (rev * 10) + lastDigit;
+        // num /= 10;
+        // }
+        // if (og == rev)
+        // System.out.println("Palindrome");
+        // else
+        // System.out.println(("Not Palindrome"));
+
+        // Scanner sc = new Scanner(System.in);
+        // int num = sc.nextInt();
+        // int pow = sc.nextInt();
+        // int result = 1;
+
+        // for (int i = 1; i <= pow; i++) {
+        // result = result * num;
+        // }
+        // System.out.println(result);
+        // checkPrime(num);
+
     }
 }
